cf44d2e7677f4bbc5fba642bb9c474bc09f9b896
Asher Glick <asher.glick@gmail.com>
2012-12-31 16:15:51 -0500

    beginning hash function to be called by other programs

--- Core/main.cpp
+++ Core/main.cpp
@@ -1,19 +1,42 @@
-#include <stdio.h>
-#include <string.h>
+#include <iostream>
+#include <iomanip>
+#include <string>
 #include <openssl/sha.h>
 
-int main()
+using namespace std;
+
+int main(int argc, char* argv[])
 {
-    unsigned char ibuf[] = "compute sha1";
-    unsigned char obuf[20];
+    string input;
 
-    SHA1(ibuf, strlen(ibuf), obuf);
+    // a sha1 hash is 20 bytes
+    unsigned char hash[20];
 
-    int i;
-    for (i = 0; i < 20; i++) {
-        printf("%02x ", obuf[i]);
+    //cout << "enter your string: ";
+    //getline(cin, input);
+  	input = "hello";
+
+    // compute the sha1 of the input, and store it our  hash array
+    SHA1((unsigned char*)input.c_str(), input.size(), hash);
+    // the above is the exact same thing as doing:
+    //    SHA_CTX ctx;
+    //    
+    //    SHA1_Init(&ctx);
+    //    SHA1_Update(&ctx, input.c_str(), input.size());
+    //    SHA1_Final(hash, &ctx);
+
+    // since the hash array just contains a bunch of bytes,
+    // print them as hexadecimal values
+    cout << "the hash was: ";
+    for(int i = 0; i < 20; ++i) {
+        cout << hex << setw(2) << setfill('0') << (int)hash[i];
     }
-    printf("\n");
+    cout << endl;
+}
 
-    return 0;
+string getPassword(string masterpass, string location) {
+	string prehash = masterpass+location;
+
+	//unsigned char hash[20];
+	return prehash;
 }
