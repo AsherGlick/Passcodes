f206c81a3e078b5634a4d0e807d79a9d173fefa2
Asher Glick <asher.glick@gmail.com>
2013-01-13 17:48:23 -0500

    going to do the base 64 conversion a very poor way to match up with the js version

--- Android/src/app/seraph/passcodes/MainActivity.java
+++ Android/src/app/seraph/passcodes/MainActivity.java
@@ -1,5 +1,9 @@
 package app.seraph.passcodes;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import android.annotation.TargetApi;
 import android.app.Activity;
 //import android.text.ClipboardManager;
@@ -12,6 +16,7 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -32,23 +37,38 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
+	// main function to call when the generate button gets pressed
 	public void generatePasscode() {
-		//android.text.ClipboardManager clipboard = (android.text.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
 		
-		//ClipData clipping = ClipData.newPlainText("simple text","PASTE ME");
-		//clipboard.setText("paste me");
-		//clipboard.setPrimaryClip(clipping);
+		EditText domainBox = (EditText) findViewById(R.id.domainBox);
+		EditText passwordBox = (EditText) findViewById(R.id.passwordBox);
+		
+		String domain = domainBox.getText().toString();
+		String password = passwordBox.getText().toString();
 		copyToClipboard("HELLO EVERYONE!");
 		
-		int duration = Toast.LENGTH_SHORT;
-		Toast toast = Toast.makeText(this, "Copied To Clipboard"+android.os.Build.VERSION.SDK_INT, duration);
+		int duration = Toast.LENGTH_LONG;
+		byte[] data = getHash(domain,password);
+		String hash = String.format("%0" + (data.length*2) + "X", new BigInteger(1, data));
+		Toast toast = Toast.makeText(this, hash, duration);
 		toast.show();
+		
+		String numbers = "";
+		for (byte b : data) {
+			numbers += (int)b + ",";
+		}
+		
+		Toast toast2 = Toast.makeText(this, numbers, duration);
+		toast2.show();
 	}
 	
-	
+	/****************************** COPY TO CLIPBOARD *****************************\
+	| This function is a wrapper for the copy to clipboard old and new functions.  |
+	| It acts as abstraction layer for the actual copy to clipboard functions'     |
+	\******************************************************************************/
 	public void copyToClipboard(String clip) {
 		// If this divice does not have the new clipboard function then use the old one
-		if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.HONEYCOMB) {
+		if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.HONEYCOMB 	) {
 			copyToClipboardOld(clip);
 		}
 		// otherwise use the new one
@@ -56,7 +76,10 @@ public class MainActivity extends Activity {
 			copyToClipboardNew (clip);
 		}
 	}
-	
+	/**************************** COPY TO CLIPBOARD NEW ***************************\
+	| This function uses the new functions for copying to the clipboard for        |
+	| android devices that have a high enough API level to handle the new method   |
+	\******************************************************************************/
 	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	public void copyToClipboardNew(String clip) {
 		android.content.ClipboardManager clipboard = (android.content.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
@@ -64,10 +87,28 @@ public class MainActivity extends Activity {
 		clipboard.setPrimaryClip(clipping);
 	}
 	
+	/**************************** COPY TO CLIPBOARD OLD ***************************\
+	| This function uses the deprecated method for copying to the clipboard for    |
+	| devices that are unable to use the new API (all pre-honycomb devices) so     |
+	| that the clipboard will still work                                           |
+	\******************************************************************************/
 	@TargetApi(Build.VERSION_CODES.ECLAIR)
 	@SuppressWarnings("deprecation")
 	public void copyToClipboardOld(String clip) {
 		android.text.ClipboardManager clipboard = (android.text.ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
 		clipboard.setText(clip);
+	}
+	
+	public byte[] getHash(String domain, String password) {
+		String concat = domain+password;
+		
+		MessageDigest digest = null; // initilize the possible digest
+		
+		try {digest = MessageDigest.getInstance("SHA-256");}
+		catch (NoSuchAlgorithmException e1) {
+			e1.printStackTrace();
+		}
+		digest.reset();
+		return digest.digest(concat.getBytes());
 	}
 }
