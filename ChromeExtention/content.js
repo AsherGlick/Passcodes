7ed6b9857146d87094f8eca92be8ae9ca75d62ce
Asher Glick <asher.glick@gmail.com>
2013-01-19 03:46:42 -0500

    updated settings for displaying input boxes

--- ChromeExtention/content.js
+++ ChromeExtention/content.js
@@ -40,23 +40,30 @@ chrome.extension.onRequest.addListener(
 			
 			var html = ""
 			html += "<style>";
-			html += ".content{background-color: #999;border-radius: 5px;padding: 5px;width: 200px;margin-left: auto;margin-right: auto;margin-top: 100px; font-family: sans-serif;}";
-			html += ".button{border-radius: 3px;background-color: #DDD;cursor:pointer;padding: 10px;border: 1px solid #CCC;margin: 0px;width: 100%;-webkit-appearance: none;}";
-			html += ".button:hover{background-color: #CCC;}";
-			html += ".textbox {border-radius: 3px;padding: 10px;border: 0px;width: 178px;margin: 0px;outline: none;margin-bottom: 2px;border: 1px solid #CCC;}";
+			html += "#__passcodes__content {}"
+			html += "#__passcodes__content .__passcodes__content{background-color: #999;border-radius: 5px;padding: 5px;width: 200px;margin-left: auto;margin-right: auto;margin-top: 100px; font-family: sans-serif;}";
+			html += "#__passcodes__content .__passcodes__button{border-radius: 3px;background-color: #DDD;cursor:pointer;padding: 10px;border: 1px solid #CCC;margin: 0px;width: 100%;-webkit-appearance: none;}";
+			html += "#__passcodes__content .__passcodes__button:hover{background-color: #CCC;}";
+			html += "#__passcodes__content .__passcodes__textbox {border-radius: 3px;padding: 10px;border: 0px;width: 100% !important;margin: 0px;outline: none;margin-bottom: 2px;border: 1px solid #CCC; box-sizing: border-box;}";
 			html += "</style>";
-			html += '<div class="content"> <form name="passcodesInput" action="javascript:showHash();" method="post" style="margin:0px">';
-			html += '<input class="textbox" type="textbox" id="website" placeholder="Website" autocomplete="off"/> <br />';
-			html += '<input class="textbox" type="password" id="masterPassword" placeholder="Password" autocomplete="off"/> <br />';
-			html += '<input class="button" type="submit" value="Generate" />';
-			html += '</form> </div>';
+			html += '<div id="__passcodes__content" style="position:static; background: #F00"> <div class="__passcodes__content" > <form name="passcodesInput" action="javascript:showHash();" method="post" style="margin:0px">';
+			html += '<input class="__passcodes__textbox" type="textbox" id="__passcodes__website" placeholder="Website" autocomplete="off"/> <br />';
+			html += '<input class="__passcodes__textbox" type="password" id="__passcodes__masterPassword" placeholder="Password" autocomplete="off"/> <br />';
+			html += '<input class="__passcodes__button" type="submit" value="Generate" />';
+			html += '</form> </div> </div>';
+
+			//alert(document.getElementById("__passcodes__website").style)
 
 			dimmer.innerHTML = html;
 
+			dimmer.onclick= function(e) {
+				//alert("CLICKED");
+				//document.body.removeChild(this);
+			}
 
 			document.body.appendChild(dimmer);
 
-			alert("hello");
+			//alert("hello");
 		
 		/*
 		if(request == "__passcod.es__getTarget") {
