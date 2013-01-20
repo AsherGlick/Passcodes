7b3f0b7b130c88048c7ca8910fc5b87d487831f8
Asher Glick <asher.glick@gmail.com>
2013-01-20 14:13:41 -0500

    created large icon and script to change it to different sizes

---
+++ generateIcons.sh
@@ -0,0 +1,4 @@
+convert keyhole.png  -resize 94x94  xhdpi.png
+convert keyhole.png  -resize 48x48  mdpi.png
+convert keyhole.png  -resize 36x36  ldpi.png
+convert keyhole.png  -resize 72x72  hdpi.png
