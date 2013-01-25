d6862a41c5d8351d16f5b2a05d260d4d3ce50480
Asher Glick <asher.glick@gmail.com>
2013-01-25 15:45:52 -0500

    updated code to allow popup, content, and background to pass messages

--- ChromeExtention/script.js
+++ ChromeExtention/script.js
@@ -8,12 +8,12 @@ function startPasscodes(info, tab) {
     chrome.tabs.sendRequest(tab.id, "__passcod.es__getTarget", function(target) {
         //alert(target);
         // var tabsWindow = chrome.windows.get(tab.windowId);
-        // console.log(tabsWindow);
+        // console.log(tabsWindow); 
         // var xcoord = 900;//tabsWindow.left + tabsWindow.width/2;
         // var ycoord = 500;
         // chrome.windows.create({'url': 'http://passcod.es', 'type': 'popup', 'width':300, 'height':200});
 
-        console.log("CLICKED");
+        //console.log("CLICKED");
 
         var w = 440;
         var h = 420;
@@ -24,8 +24,22 @@ function startPasscodes(info, tab) {
 
         var newpopup = chrome.windows.create({'url': 'popup.html','focused':true ,'type': 'popup', 'width': w, 'height': h, 'left': left, 'top': top} , function(window) {
         });
-
+        console.log(newpopup);
 
 
     });
 }
+
+
+// Listen for a connection from the popup asking for informaion
+chrome.extension.onConnect.addListener(function(port) {
+  console.assert(port.name == "knockknock");
+  port.onMessage.addListener(function(msg) {
+    if (msg.joke == "Knock knock")
+      port.postMessage({question: "Who's there?"});
+    else if (msg.answer == "Madame")
+      port.postMessage({question: "Madame who?"});
+    else if (msg.answer == "Madame... Bovary")
+      port.postMessage({question: "I don't get it."});
+  });
+});
--- ChromeExtention/security-loop.js
+++ ChromeExtention/security-loop.js
@@ -1,1 +1,15 @@
-document.getElementById('form').onsubmit = function() { console.log("hello") };
+// this page runs all the script that should be run on onload but cant becasue this is a chrome plugin
+var port = chrome.extension.connect({name: "popupConnection"});
+
+port.postMessage({query:"domain"}); // request the domain from the tab
+
+port.onMessage.addListener(function(msg) {
+  if (msg.query == "domain")
+    // set domain to msg.responce
+  else if (msg.query == "done")
+  	//sucessfully recived master password
+  	// close popup
+ });
+
+
+document.getElementById('form').onsubmit = function() { port.postMessage({'password':generatePassword()})};
