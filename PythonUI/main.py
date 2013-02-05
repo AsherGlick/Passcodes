6b78402e7027a41194dbaf3f28b39e98f4ce9706
Asher Glick <asher.glick@gmail.com>
2013-02-05 15:28:25 -0500

    removed some comments from the code this used to be a part of

--- PythonUI/main.py
+++ PythonUI/main.py
@@ -1,17 +1,12 @@
 import sys, random, time
 from PyQt4 import QtGui, QtCore
 
-################################ MAIN QT WIDGET ################################
-# The main qt widget handles all of the ui inside of the main window. This     #
-# mainly includes the two list views displaying all of the students as well    #
-# as just the students selected. Allong with the ability to search those lists #
-################################################################################
+
 class mainWidget(QtGui.QWidget):
 	def __init__(self):
 		super(mainWidget, self).__init__()
 		self.initUI()
 
-	## this function intilizes the UI for the main widget which currently involves seting up two lists that get written to
 	def initUI(self):
 		self.resize(600, 500)
 		self.setWindowTitle("Passcod.es Desktop Application")
