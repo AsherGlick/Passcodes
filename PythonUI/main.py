4aceb293394b62cf80505a28c64ff9efe91afdbc
Asher Glick <asher.glick@gmail.com>
2013-02-07 14:59:48 -0500

    begining to change layout to support output as well as input

--- PythonUI/main.py
+++ PythonUI/main.py
@@ -2,6 +2,10 @@ import sys, random, time
 from PyQt4 import QtGui, QtCore
 
 
+
+def generatePassword(domain, password):
+	return "deadbeef"
+
 #class mainWidget(QtGui.QMainWindow):
 class mainWidget(QtGui.QWidget):
 	def __init__(self):
@@ -16,51 +20,43 @@ class mainWidget(QtGui.QWidget):
 		self.resize(600, 500)
 		self.setWindowTitle("Passcod.es Desktop Application")
 
+
+		
 		self.hlayout = QtGui.QHBoxLayout()
 		self.hlayout.addStretch(1)
-
 		self.vlayout = QtGui.QVBoxLayout()
 		self.vlayout.addStretch(1)
-		#ui = QtGui.QWidget()
-
-
-
-
+		# Create the domain text box
 		self.domain = QtGui.QLineEdit("",self)
 		self.domain.setPlaceholderText("Domain")
 		self.vlayout.addWidget(self.domain)
-
+		# Create the password text box
 		self.password = QtGui.QLineEdit("",self)
 		self.password.setPlaceholderText("Password")
 		self.vlayout.addWidget(self.password)
-
-
+		# Create the show password button
 		self.displayPasswordButton = QtGui.QPushButton("Show Password", self)
 		self.displayPasswordButton.clicked.connect(self.displayPassword)
 		self.vlayout.addWidget(self.displayPasswordButton)
-
-
+		# Create the copy password to clipboard button
 		self.copyPasswordButton = QtGui.QPushButton("Copy Password to Clipboard", self)
 		self.copyPasswordButton.clicked.connect(self.copyPassword)
 		self.vlayout.addWidget(self.copyPasswordButton)
-
-
-		#ui.setLayout(self.vlayout)
-
-		self.hlayout.addLayout(self.vlayout)
-
-		self.hlayout.addStretch(1)
-
-		self.setLayout(self.hlayout)
+		# Finish configuring the layput
 		self.vlayout.addStretch(1)
-		#self.setCentralWidget(ui)
-		#QtGui.QApplication.setStyle(QtGui.QStyleFactory.create('Cleanlooks'))
+		self.hlayout.addLayout(self.vlayout)
+		self.hlayout.addStretch(1)
+		# Set the layout to the hlayout
+		self.setLayout(self.hlayout)
 
-	def displayPassword():
+
+	def displayPassword(self):
 		pass
 
-	def copyPassword():
+	def copyPassword(self):
 		pass
+
+
 
 
 
