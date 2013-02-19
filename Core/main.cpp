bfc2c8432b6f74aa143d190e8f49101efc983c0b
Asher Glick <asher.glick@gmail.com>
2013-02-18 23:25:59 -0500

    added a method to do fast conversion between byes and a base 64 string

--- Core/main.cpp
+++ Core/main.cpp
@@ -6,43 +6,63 @@
 using namespace std;
 #define HASHSIZE 32
 
-int main(int argc, char* argv[])
-{
-    string input;
+string getCutHex(unsigned char hash[HASHSIZE]) {
+	unsigned char b64_1 = hash[0] >> 2;
+	unsigned char b64_2 = ((hash[0]&0x03)<<4) + (hash[1]>>4);
+	unsigned char b64_3 = ((hash[1]&0x0F)<<2) + (hash[2]>>6);
+	unsigned char b64_4 = hash[2] & 0x3F;
+	cout << (int)0x3F << endl;
+	cout << (int)hash[0] << ":" << (int)hash[1] << ":" << (int)hash[2] << endl;
+	cout << (int)b64_1 << ":" << (int)b64_2 << ":" << (int)b64_3 << ":" << (int)b64_4 << endl;
 
-    // a sha1 hash is 20 bytes
-    unsigned char hash[HASHSIZE];
-
-    for (int i = 0; i < HASHSIZE; i++) {
-        hash[i] = 'x';
-    }
-
-    //cout << "enter your string: ";
-    //getline(cin, input);
-  	input = "hello";
-
-    // compute the sha1 of the input, and store it our  hash array
-    SHA256((unsigned char*)input.c_str(), input.size(), hash);
-    // the above is the exact same thing as doing:
-    //    SHA_CTX ctx;
-    //    
-    //    SHA1_Init(&ctx);
-    //    SHA1_Update(&ctx, input.c_str(), input.size());
-    //    SHA1_Final(hash, &ctx);
-
-    // since the hash array just contains a bunch of bytes,
-    // print them as hexadecimal values
-    cout << "the hash was: ";
-    for(int i = 0; i < HASHSIZE; ++i) {
-        cout << hex << setw(2) << setfill('0') << (int)hash[i];
-    }
-    cout << endl;
-    cout << "DONE!" << endl;
+	return "";
 }
 
-string getPassword(string masterpass, string location) {
-	string prehash = masterpass+location;
+string getPassword(string masterpass, string domain) {
+	string prehash = masterpass+domain;
+	unsigned char hash[HASHSIZE];
+	SHA256((unsigned char*)prehash.c_str(), prehash.size(), hash);
+
+	getCutHex(hash);
 
 	//unsigned char hash[20];
 	return prehash;
+}
+
+
+
+int main(int argc, char* argv[]) {
+	string input;
+
+	// a sha1 hash is 20 bytes
+
+
+	// for (int i = 0; i < HASHSIZE; i++) {
+	// 	hash[i] = 'x';
+	// }
+
+	//cout << "enter your string: ";
+	//getline(cin, input);
+	input = "hello";
+
+	// compute the sha1 of the input, and store it our  hash array
+
+	// the above is the exact same thing as doing:
+	//    SHA_CTX ctx;
+	//    
+	//    SHA1_Init(&ctx);
+	//    SHA1_Update(&ctx, input.c_str(), input.size());
+	//    SHA1_Final(hash, &ctx);
+
+	// since the hash array just contains a bunch of bytes,
+	// print them as hexadecimal values
+	// cout << "the hash was: ";
+	// for(int i = 0; i < HASHSIZE; ++i) {
+	// 	cout << hex << setw(2) << setfill('0') << (int)hash[i];
+	// }
+	// cout << endl;
+
+	getPassword ("harvy","dent");
+
+	cout << "DONE!" << endl;
 }
