baca75c4e9c45ce4af69de1f22582c26044cdd65
Asher Glick <asher.glick@gmail.com>
2013-03-04 16:40:37 -0500

    added comments to the functions in script.js

--- ChromeExtention/script.js
+++ ChromeExtention/script.js
@@ -1,7 +1,20 @@
+/************************************ ABOUT ***********************************\
+| This script is the background script. It handles the context menu item       |
+| being clicked and a single message interface returning the tab that last     |
+| opened the passcodes application.                                            |
+\******************************************************************************/
+
 var passwordGenerator = chrome.contextMenus.create({"title":"Fill In Passcode Here", "contexts":["editable"],"onclick":startPasscodes});
 
 var mostRecentActiveTab = null;
 
+
+/******************************* START PASSCODES ******************************\
+| This function is run when the context menu item is clicked. It saves the ID  |
+| of the tab that the context menu was clicked in and then opens a new popup   |
+| window containing the passcod.es interface for the user to type in their     |
+| master password                                                              |
+\******************************************************************************/
 function startPasscodes(info, tab) {
 	//insertAtCaret("HELLO");
 	//alert(JSON.stringify(info));
@@ -35,7 +48,12 @@ function startPasscodes(info, tab) {
     //});
 }
 
-// add a listener for when the popup requests the tab id
+/******************************** RETURN TAB ID *******************************\
+| This function is a listener for when the popup requests the ID of the tab    |
+| that opened it. This function returns the ID of the last tab that was        |
+| opened and then erases the value so that if it is called again it will not   |
+| return the same tab twice without the tab openeing the menu twice            |
+\******************************************************************************/
 chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.request == "tabid") {
