8221a8b3a57eb784be9e0a7512b6452a1c26499c
Asher Glick <asher.glick@gmail.com>
2013-03-04 16:43:42 -0500

    removed extra lines and spaces

--- ChromeExtention/script.js
+++ ChromeExtention/script.js
@@ -3,11 +3,10 @@
 | being clicked and a single message interface returning the tab that last     |
 | opened the passcodes application.                                            |
 \******************************************************************************/
-
-var passwordGenerator = chrome.contextMenus.create({"title":"Fill In Passcode Here", "contexts":["editable"],"onclick":startPasscodes});
-
 var mostRecentActiveTab = null;
 
+// Regester the start Passcodes function as a context menu item
+var passwordGenerator = chrome.contextMenus.create({"title":"Fill In Passcode Here", "contexts":["editable"],"onclick":startPasscodes});
 
 /******************************* START PASSCODES ******************************\
 | This function is run when the context menu item is clicked. It saves the ID  |
@@ -16,36 +15,18 @@ var mostRecentActiveTab = null;
 | master password                                                              |
 \******************************************************************************/
 function startPasscodes(info, tab) {
-	//insertAtCaret("HELLO");
-	//alert(JSON.stringify(info));
-	//alert(JSON.stringify(tab));
-	//background
+    // Save the Tab that the context menu was clicked on
     mostRecentActiveTab = tab.id;
+    // Define the size and position of the new popup window
+    var w = 440;
+    var h = 420;
+    var left = (screen.width/2)-(w/2);
+    var top =(screen.height/2)-(h/2); 
+    // Create the popup
+    var newpopup = chrome.windows.create({'url': 'popup.html','focused':true ,'type': 'popup', 'width': w, 'height': h, 'left': left, 'top': top} , function(window) {});
 
-    //chrome.tabs.sendRequest(tab.id, "__passcod.es__getTarget", function(target) {
-        //alert(target);
-        // var tabsWindow = chrome.windows.get(tab.windowId);
-        // console.log(tabsWindow); 
-        // var xcoord = 900;//tabsWindow.left + tabsWindow.width/2;
-        // var ycoord = 500;
-        // chrome.windows.create({'url': 'http://passcod.es', 'type': 'popup', 'width':300, 'height':200});
-
-        //console.log("CLICKED");
-
-        var w = 440;
-        var h = 420;
-        var left = (screen.width/2)-(w/2);
-        var top =(screen.height/2)-(h/2); 
-
-
-
-        var newpopup = chrome.windows.create({'url': 'popup.html','focused':true ,'type': 'popup', 'width': w, 'height': h, 'left': left, 'top': top} , function(window) {
-        });
-
-        console.log(newpopup);
-
-
-    //});
+    // Debug logging
+    console.log(newpopup);
 }
 
 /******************************** RETURN TAB ID *******************************\
