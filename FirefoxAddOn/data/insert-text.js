3b3a78ec01f3a9f9078a932e0ec64b99a20fda84
Asher Glick <asher.glick@gmail.com>
2013-06-11 14:13:26 -0400

    implemented functionality to insert text in firefox

--- FirefoxAddOn/data/insert-text.js
+++ FirefoxAddOn/data/insert-text.js
@@ -1,3 +1,21 @@
 self.on("click", function (node, data) {
-	self.postMessage(node.src);
+	//self.postMessage(node.src);
+
+	var text = "Hello There!";
+
+	var txtarea = node;
+	var scrollPos = txtarea.scrollTop;
+	var strPos = 0;
+	strPos = txtarea.selectionStart;
+
+	var front = (txtarea.value).substring(0,strPos);
+	var back = (txtarea.value).substring(strPos,txtarea.value.length);
+
+	txtarea.value=front+text+back;
+	strPos = strPos + text.length;
+
+	txtarea.selectionStart = strPos;
+	txtarea.selectionEnd = strPos;
+	txtarea.focus();
+	txtarea.scrollTop = scrollPos;
 });
