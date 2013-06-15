c4d16c18d596b1b283dd00b2609817542107eb8a
Asher Glick <asher.glick@gmail.com>
2013-06-15 03:53:24 -0400

    added commit message option

--- Web/ghpublish.sh
+++ Web/ghpublish.sh
@@ -1,5 +1,10 @@
 TMPDIR=`mktemp -d`
 amassite amassite/ $TMPDIR -C
 echo "$TMPDIR"
+read -p "Commit Message " MESSAGE 
 cd ..
 git checkout gh-pages
+#git add *
+git status
+#git commit -m "$MESSAGE"
+git checkout master
