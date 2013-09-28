d8bdb3cf6958785f54e38e5084cb7808970786ec
Asher Glick <asher.glick@gmail.com>
2013-09-28 00:19:08 -0400

    updated the api to allow for multiple requests at the same time also added prettyprint formatting of json

--- WebApi/v1/api.php
+++ WebApi/v1/api.php
@@ -20,7 +20,7 @@ if ($request[0] == 'domain') {
 }
 
 
-function getRules ($domain, $inifile) {
+function getRules ($domains, $inifile) {
 	$mysqli = new mysqli($inifile['url'], $inifile['username'], $inifile['password'], $inifile['database']) ;
 
 	/* check connection */
@@ -29,20 +29,101 @@ function getRules ($domain, $inifile) {
 	    exit();
 	}
 
-	$domain = $mysqli->escape_string($domain);
+	$domains = explode(' ', $domains);
+	$domainList = "";
+	foreach ($domains as &$domain) {
+		$domain = $mysqli->escape_string($domain);
+		$domainList .= "'".$domain."',";
+	}
+	$domainList = substr($domainList,0,-1);
 
-	$sql = "SELECT name, version, creationdate, minlength, maxlength, restrictedcharacters, regex, parent from " . $inifile['rulesTable'] . " WHERE (name, version) IN (SELECT name, MAX(version) FROM rules WHERE name in ('" . $domain . "', 'example') GROUP BY name)";
 
+	# build and execute the sql query
+	$sql = "SELECT name, version, creationdate, minlength, maxlength, restrictedcharacters, regex, parent from " . $inifile['rulesTable'] . " WHERE (name, version) IN (SELECT name, MAX(version) FROM rules WHERE name in (" . $domainList . ") GROUP BY name)";
 	$result = $mysqli->query($sql) or die("Unable to select: " . $mysqli->error );
 
-
+	# get the results of the sql query
 	$rows = array();
 	while($row = $result->fetch_assoc()) {
 	    $rows[] = $row;
 	}
-	print_r ($rows);
-	print "\n";
-	print json_encode($rows);
+
+	# build list of missing data from query
+	foreach ($rows as &$row) {
+		$rowname = $row["name"];
+		if(($key = array_search($rowname, $domains)) !== false) {
+		    unset($domains[$key]);
+		}
+	}
+
+	# fill in empty data from query
+	foreach ($domains as &$domain) {
+		$newrow = Array();
+		$newrow['name']=$domain;
+		$newrow['version']='0';
+		array_push($rows, $newrow);
+	}
+	print indent(json_encode($rows));
+}
+
+
+
+/**
+ * Indents a flat JSON string to make it more human-readable.
+ *
+ * @param string $json The original JSON string to process.
+ *
+ * @return string Indented version of the original JSON string.
+ */
+function indent($json) {
+
+    $result      = '';
+    $pos         = 0;
+    $strLen      = strlen($json);
+    $indentStr   = '  ';
+    $newLine     = "\n";
+    $prevChar    = '';
+    $outOfQuotes = true;
+
+    for ($i=0; $i<=$strLen; $i++) {
+
+        // Grab the next character in the string.
+        $char = substr($json, $i, 1);
+
+        // Are we inside a quoted string?
+        if ($char == '"' && $prevChar != '\\') {
+            $outOfQuotes = !$outOfQuotes;
+
+        // If this character is the end of an element,
+        // output a new line and indent the next line.
+        } else if(($char == '}' || $char == ']') && $outOfQuotes) {
+            $result .= $newLine;
+            $pos --;
+            for ($j=0; $j<$pos; $j++) {
+                $result .= $indentStr;
+            }
+        }
+
+        // Add the character to the result string.
+        $result .= $char;
+
+        // If the last character was the beginning of an element,
+        // output a new line and indent the next line.
+        if (($char == ',' || $char == '{' || $char == '[') && $outOfQuotes) {
+            $result .= $newLine;
+            if ($char == '{' || $char == '[') {
+                $pos ++;
+            }
+
+            for ($j = 0; $j < $pos; $j++) {
+                $result .= $indentStr;
+            }
+        }
+
+        $prevChar = $char;
+    }
+
+    return $result;
 }
 
 ?>
