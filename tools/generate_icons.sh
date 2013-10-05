b41b7d51c77f0f7f609cade222046efd11c0292f
Asher Glick <asher.glick@gmail.com>
2013-10-05 16:24:12 -0400

    updated the icons script and the icons

--- tools/generate_icons.sh
+++ tools/generate_icons.sh
@@ -1,6 +1,20 @@
 #! /bin/sh
-GITROOT=`git rev-parse --show-toplevel`;
-echo $GITROOT
+##################################### ABOUT ####################################
+# This file takes the svg 'icon.svg' and generates png images of different     #
+# sizes for the different passcodes applications                               #
+################################################################################
 
-# Generate Firefox Icons
-rsvg-convert --width 94 --height 94 icon.svg --output "$GITROOT/source/94.png"
+
+GITROOT=`git rev-parse --show-toplevel`;
+
+# Generate Android Icons
+rsvg-convert --width 512 --height 512 icon.svg --output "$GITROOT/source/Android/ic_launcher-web.png"
+rsvg-convert --width 94 --height 94 icon.svg --output "$GITROOT/source/Android/res/drawable-xhdpi/ic_launcher.png"
+rsvg-convert --width 72 --height 72 icon.svg --output "$GITROOT/source/Android/res/drawable-hdpi/ic_launcher.png"
+rsvg-convert --width 48 --height 48 icon.svg --output "$GITROOT/source/Android/res/drawable-mdpi/ic_launcher.png"
+rsvg-convert --width 36 --height 36 icon.svg --output "$GITROOT/source/Android/res/drawable-ldpi/ic_launcher.png"
+
+# Generate Chrome Addon Icons
+rsvg-convert --width 16 --height 16 icon.svg --output "$GITROOT/source/ChormeExtention/icon-16.png"
+rsvg-convert --width 48 --height 48 icon.svg --output "$GITROOT/source/ChormeExtention/icon-48.png"
+rsvg-convert --width 128 --height 128 icon.svg --output "$GITROOT/source/ChormeExtention/icon-128.png"
