3a99d73308cab2c5f21883cab66a8d0020aec7ad
Asher Glick <asher.glick@gmail.com>
2013-12-07 12:23:55 -0500

    modified the layout of the Domain settings template to better adhear to standards

--- source/Core/templates/DomainSettings.h
+++ source/Core/templates/DomainSettings.h
@@ -1,24 +1,25 @@
 #ifndef __DOMAIN_SETTINGS_H__
 #define __DOMAIN_SETTINGS_H__
 
+#include <string>
+
 class DomainSettings {
-public:
+  public:
     const string SQLCreateTable = [[SQLCREATETABLE]];
     const unsinged int columnCount = [[SQLCOLUMNCOUNT]];
     [[CPPVARIABLES]]
 
-    
-    DomainSettings( std::string domain ) {
+    explicit DomainSettings(std::string domain) {
         this->domain = domain;
         this->allowedCharacters = " !\"#$%&'()*+,-./1234567890:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"; // every typeable ascii character
         this->maxCharacters = 16;
-        this->regex = ".*"; // Match anything
+        this->regex = ".*";  // Match anything
     }
 
     // Initilizer from SQL
-    DomainSettings( sqlite3_stmt *statement ) {
+    explicit DomainSettings(sqlite3_stmt *statement) {
         // do a quick check to make sure the database schema has not changed
-        if (sqlite3_column_count(statement) != columnCount) { 
+        if (sqlite3_column_count(statement) != columnCount) {
             fprintf(stderr, "The databse schema vseems to be incorrect, maybe this should be cheked\n");
             fprintf(stderr, "The version of the software or database may be out of date\n");
         }
