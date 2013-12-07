e5997e21b3c2c82238f838f2b38d72e032fae2a3
Asher Glick <asher.glick@gmail.com>
2013-12-07 14:27:46 -0500

    added a new function to produce allowed characters form the restricted characters list changed the way that the static variables are stored in the class

--- source/Core/templates/DomainSettings.h
+++ source/Core/templates/DomainSettings.h
@@ -5,14 +5,15 @@
 
 class DomainSettings {
   public:
-    const string SQLCreateTable = [[SQLCREATETABLE]];
-    const unsinged int columnCount = [[SQLCOLUMNCOUNT]];
+    // static std::string SQLCreateTable = [[SQLCREATETABLE]];
+    const static int columnCount = [[SQLCOLUMNCOUNT]];
     [[CPPVARIABLES]]
 
     explicit DomainSettings(std::string domain) {
         this->domain = domain;
-        this->allowedCharacters = " !\"#$%&'()*+,-./1234567890:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"; // every typeable ascii character
-        this->maxCharacters = 16;
+        this->restrictedCharacters =""; 
+         // = " !\"#$%&'()*+,-./1234567890:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"; // every typeable ascii character
+        this->maxLength = 16;
         this->regex = ".*";  // Match anything
     }
 
@@ -26,9 +27,31 @@ class DomainSettings {
 
 
         for (int i = 0; i < sqlite3_column_count(statement); i++) {
+            std::string columnName = std::string(reinterpret_cast<const char*> (*sqlite3_column_table_name(statement, i)));
             [[CPPCONSTRUCTOR]]
         }
+    }
+
+    std::string CreateTableSQL () {
+        return [[SQLCREATETABLE]];
+    }
+
+    //" !\"#$%&'()*+,-./1234567890:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+    std::string allowedCharacters() {
+        char restrictedCharactersMask[95] = {0};
+        for (char character : this->restrictedCharacters) {
+            restrictedCharactersMask[character-32] = 1;
+        }
+        std::string allowedCharacters = "";
+        for (int i = 0; i < 95; i++) {
+            if (restrictedCharactersMask[i] == 0) {
+                allowedCharacters += char(i+32);
+            }
+        }
+        return allowedCharacters;
     }
 };
 
 #endif
+
+// !\"#$%&'()*+,-./1234567890:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~
