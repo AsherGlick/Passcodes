6c896de42eabdba4462978d169fbe034c5f05bd5
Asher Glick <asher.glick@gmail.com>
2014-08-24 17:16:24 -0400

    added a template for future javascript functionality to handle domain settings

---
+++ source/Core/templates/DomainSettings.js
@@ -0,0 +1,20 @@
+// var DomainSettings = DomainSettings || (function (Math, undefined) { // dont think that I need math but undefined might be useful (maybe)
+// 	// Insert the settings variables here
+
+
+// 	var 
+// }(Math)); // Just as the comment at the top states the math library is probably not nessasary
+
+
+
+// Ok so this seems to be creating a namespace not just a class
+// that would be useful for other things but I dont think that it is what I want for domain settings
+
+var DomainSettings = function(undefined) {
+	[[JSVARIABLES]]
+	// JSON PARSING or something?
+};
+
+
+
+// How to store data from a firefox extention?
